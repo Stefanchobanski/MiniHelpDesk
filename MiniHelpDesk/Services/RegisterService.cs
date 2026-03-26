554753a663b2002838a41555ae1567999cd34dde
StefanChobanski <stefanchobanski@gmail.com>
2026-03-26 20:24:23 +0200

    add file with gitignore

---
+++ MiniHelpDesk/Services/RegisterService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniHelpDesk.Services;
+
+public class RegisterService
+{
+}
