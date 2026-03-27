794aed7b629584106d04c5dccc9afd207ae915ab
kzwskii <alioioioio@icloud.com>
2026-03-27 16:47:07 +0200

    Test

--- MiniHelpDesk/App/Models/Comment.cs
+++ MiniHelpDesk/App/Models/Comment.cs
@@ -13,6 +13,6 @@ namespace App.Models
         public int UserID { get; set; }
         public string Text { get; set; } = null!;
         public DateTime CreatedDate { get; set; }
-
+        // Test
     }
 }
