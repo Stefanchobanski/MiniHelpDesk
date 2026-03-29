ac9710a71dde27b0bbf80ddc28aeae9c52be3a53
alex k <161456021+kzwskii@users.noreply.github.com>
2026-03-29 17:41:01 +0200

    IRepository, IUserRepository, UserContext added

--- MiniHelpDesk/App/RegisterEventHelpers.cs
+++ MiniHelpDesk/App/RegisterEventHelpers.cs
@@ -4,16 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace App
+namespace App;
+
+public static class RegisterEventHelpers
 {
-    public static class RegisterEventHelpers
+    public static bool CheckAllFields(string txtUsername, string txtEmail, string txtPassword, string txtConfirmPassword)
     {
-        public static bool CheckAllFields(string txtUsername, string txtEmail, string txtPassword, string txtConfirmPassword)
-        {
-            return string.IsNullOrWhiteSpace(txtUsername) ||
-                   string.IsNullOrWhiteSpace(txtEmail) ||
-                   string.IsNullOrWhiteSpace(txtPassword) ||
-                   string.IsNullOrWhiteSpace(txtConfirmPassword);
-        }
+        return string.IsNullOrWhiteSpace(txtUsername) ||
+               string.IsNullOrWhiteSpace(txtEmail) ||
+               string.IsNullOrWhiteSpace(txtPassword) ||
+               string.IsNullOrWhiteSpace(txtConfirmPassword);
     }
 }
--- MiniHelpDesk/App/Repositories/IRepository.cs
+++ MiniHelpDesk/App/Repositories/IRepository.cs
@@ -4,8 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MiniHelpDesk.Repositories;
+namespace App.Repositories;
 
-public interface IRepository
+public interface IRepository<TEntity> where TEntity : class
 {
+    IEnumerable<TEntity> GetAll();
+    TEntity GetById(int id);
+    void Add(TEntity entity);
+    void Update(TEntity entity);
+    bool Delete(int id);
 }
