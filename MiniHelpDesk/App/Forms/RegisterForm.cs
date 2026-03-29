6f22b180533e25641718f94be7dd0fb075f10dfe
alex k <161456021+kzwskii@users.noreply.github.com>
2026-03-29 15:30:30 +0200

    Basic form validation for registration

--- MiniHelpDesk/App/Forms/RegisterForm.cs
+++ MiniHelpDesk/App/Forms/RegisterForm.cs
@@ -24,7 +24,31 @@ namespace App
 
         private void btnRegister_Click(object sender, EventArgs e)
         {
+            string username = txtUsername.Text;
+            string email = txtEmail.Text;
+            string password = txtPassword.Text;
+            string confirmPassword = txtConfirmPassword.Text;
 
+            if (RegisterEventHelpers.CheckAllFields(username, email, password, confirmPassword))
+            {
+                MessageBox.Show("Please fill in all fields.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!email.Contains("@") || !email.Contains("."))
+            {
+                MessageBox.Show("Please enter a valid email address.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (password != confirmPassword)
+            {
+                MessageBox.Show("Passwords do not match.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if(password.Length < 6 && confirmPassword.Length < 6)
+            {
+                MessageBox.Show("Password must be at least 6 characters long.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
         }
 
     }
