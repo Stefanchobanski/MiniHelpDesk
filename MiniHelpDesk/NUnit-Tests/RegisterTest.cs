f3fe80abd1f193f131836818da276e55d74548f6
StefanChobanski <stefanchobanski@gmail.com>
2026-04-03 00:16:51 +0300

    addNunit file test

---
+++ MiniHelpDesk/NUnit-Tests/RegisterTest.cs
@@ -0,0 +1,18 @@
+﻿using App;
+
+namespace NUnit_Tests;
+
+public class RegisterTest
+{
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
