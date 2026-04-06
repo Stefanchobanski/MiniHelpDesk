544e4fd4fcd4365e8bb827d6e3a9d7612b625e68
StefanChobanski <stefanchobanski@gmail.com>
2026-04-06 19:08:44 +0300

    create
    make enum and start model

---
+++ MiniHelpDesk/App/Models/Enums/Status.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Models.Enums;
+
+public enum Status
+{
+    New,
+    InProres,
+    Waiting,
+    Resolved,
+    Closed
+}
---
+++ MiniHelpDesk/App/Models/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using App.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Modelsö
+{
+    public class Ticket
+    {
+        public int TicketId {  get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Status Status { get; set; }
+        public Priority Priority { get; set; }
+        public DateTime CreatedDay {  get; set; }
+        public Category Category { get; set; }
+    }
+}
