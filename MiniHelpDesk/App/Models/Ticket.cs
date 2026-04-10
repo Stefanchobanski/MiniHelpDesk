2b27b2fd960be55b83845a4e65a3fbaa271defbd
StefanChobanski <stefanchobanski@gmail.com>
2026-04-10 11:23:28 +0300

    Update Ticket.cs

--- MiniHelpDesk/App/Models/Ticket.cs
+++ MiniHelpDesk/App/Models/Ticket.cs
@@ -1,4 +1,5 @@
 ﻿using App.Models.Enums;
+using Microsoft.VisualBasic.ApplicationServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,16 +7,24 @@ using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace App.Modelsö
+namespace App.Models;
+
+public class Ticket
 {
-    public class Ticket
-    {
-        public int TicketId {  get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public Status Status { get; set; }
-        public Priority Priority { get; set; }
-        public DateTime CreatedDay {  get; set; }
-        public Category Category { get; set; }
-    }
+    public int TicketId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public Status Status { get; set; }
+    public Priority Priority { get; set; }
+    public DateTime CreatedDay { get; set; }
+    public Category Category { get; set; }
+
+    public int RequesterId { get; set; }
+    public User Requester { get; set; }
+
+    public int? TechnicianId { get; set; }
+    public User? Technician { get; set; }
+
+    public List<Comment> Comments { get; set; } = new List<Comment>();
+    public List<Attachment> Attachments { get; set; } = new List<Attachment>();
 }
