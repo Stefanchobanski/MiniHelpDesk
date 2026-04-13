d7ad0576f86594e4bb3d035640aa71f46e3973e1
GhostSyntom <ghostsyntom@gmail.com>
2026-04-13 16:49:23 +0300

    Create Audit_Log.cs

---
+++ MiniHelpDesk/App/Models/Audit_Log.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    internal class Audit_Log
+    {
+        [Key]
+        public string AuditLogId { get; set; }
+
+        [Required]
+        public string Field { get; set; }
+
+        public string OldValue { get; set; }
+        public string NewValue { get; set; }
+
+        public DateTime ChangedDate { get; set; }
+
+        public string ChangedByUserId { get; set; }
+        public string TicketId { get; set; }
+    }
+}
