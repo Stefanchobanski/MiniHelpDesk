bb4de69598559e28b2e15f50bcc4f8b28d7d9773
StefanChobanski <stefanchobanski@gmail.com>
2026-04-14 08:31:36 +0300

    add files

--- MiniHelpDesk/App/Forms/AdminForm.cs
+++ MiniHelpDesk/App/Forms/AdminForm.cs
@@ -22,5 +22,6 @@ namespace App.Forms
         {
 
         }
+
     }
 }
---
+++ MiniHelpDesk/App/Models/DTOs/UserRoleDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Models.DTOs;
+
+public class UserRoleDTO
+{
+    public int UserId {  get; set; }
+    public string UserName {  get; set; }
+    public string Email {  get; set; }
+    public string Role {  get; set; }
+    public int RoleId { get; set; }
+
+    public string Display {  get; set; }
+}
