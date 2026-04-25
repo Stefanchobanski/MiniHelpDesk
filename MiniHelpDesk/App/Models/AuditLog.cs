ee582bfe03d45a8d94178001cad42c1486d663c3
GhostSyntom <ghostsyntom@gmail.com>
2026-04-26 01:01:41 +0300

    kk

---
+++ MiniHelpDesk/App/Models/AuditLog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    public class AuditLog
+    {
+        public string AuditLogId { get; set; }
+        public string Field { get; set; }
+
+        public string OldValue { get; set; }
+        public string NewValue { get; set; }
+
+        public DateTime ChangedDate { get; set; }
+
+        public string ChangedByUserId { get; set; }
+        public string TicketId { get; set; }
+
+    }
+}
---
+++ MiniHelpDesk/App/Repositories/interfaces/IAuditLogService.cs
@@ -0,0 +1,20 @@
+﻿using App.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repositories.interfaces
+{
+    public interface IAuditLogService
+    {
+        Task<AuditLogDto?> GetByIdAsync(string id);
+        Task<IEnumerable<AuditLogDto>> GetByTicketIdAsync(string ticketId);
+        Task<IEnumerable<AuditLogDto>> GetByUserIdAsync(string userId);
+        Task<IEnumerable<AuditLogDto>> GetFilteredAsync(AuditLogFilterDto filter);
+        Task<AuditLogDto> CreateAsync(CreateAuditLogDto dto);
+        Task<IEnumerable<AuditLogDto>> CreateBulkAsync(IEnumerable<CreateAuditLogDto> dtos);
+        Task DeleteByTicketIdAsync(string ticketId);
+    }
+}
